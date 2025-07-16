8124e5aff329ab005dcfce798996bcc25ab1007e
ritika7893 <ritikakumari87344@gmail.com>
2025-07-16 11:33:51 +0530

    Password Generator

---
+++ passwordgenerator/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [length, setLength] = useState(8)
+  const [numberAllowed, setNumberAllowed] = useState(false)
+  const [charAlllowed, setCharAllowed] = useState(false)
+  comst[password, setPassword] = useState("")
+  return (
+    <>
+
+    </>
+  )
+}
+
+export default App
